d10538e2d2ef6d12540fa34edf559cf4e1a47acc
paul <paul_lodder@live.nl>
2021-10-12 10:10:49 +0200

    Add setup instructions and readme

---
+++ setup.py
@@ -0,0 +1,12 @@
+import setuptools
+
+
+setuptools.setup(
+    name="setup",  # Replace with your own username
+    version="0.0.1",
+    author="meis",
+    author_email="",
+    # packages=setuptools.find_packages("src/"),
+    package_dir={"src": "src"},
+    description="Just some functions we need for this project",
+)
