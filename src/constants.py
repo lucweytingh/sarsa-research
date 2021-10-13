b1ee90111e573437982bd93099835c79bc97798a
jeroen <jeroenpjagt@gmail.com>
2021-10-13 13:00:33 +0200

    remove circular import

--- src/constants.py
+++ src/constants.py
@@ -1,12 +1,9 @@
 from src.envs.windy_gridworld import WindyGridworldEnv
 from src.envs.blackjack import BlackjackEnv
 from src.envs.gridworld import GridworldEnv
-from src.sarsa import sarsa, expected_sarsa
 
 NAME2ENV = {
     "windygridworld": WindyGridworldEnv,
     "blackjack": BlackjackEnv,
     "gridworld": GridworldEnv,
 }
-
-NAME2ALG = {"expected_sarsa": expected_sarsa, "sarsa": sarsa}
